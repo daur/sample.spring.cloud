2c809fed166916484e505588f644dcd518534b60
liwei.e <liwei.e@ideacome.com>
2018-11-27 10:05:22 +0800

    2018-11-27 by laosan
    update structure

--- spring.cloud.eureka.server/src/main/java/com/learning/sample/spring/cloud/eureka/server/EurekaServiceApplication.java
+++ spring.cloud.eureka.server/src/main/java/com/learning/sample/spring/cloud/eureka/server/EurekaServiceApplication.java
@@ -1,4 +1,4 @@
-package com.learning.springboot.sample.eureka.server;
+package com.learning.sample.spring.cloud.eureka.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
