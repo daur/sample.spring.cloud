8170a3084c17387cb8af8ba33130933b378962f7
liwei.e <liwei.e@ideacome.com>
2018-11-29 11:10:18 +0800

    2018-11-29 by laosan
    add config server

--- spring.cloud.eureka.client.service.consumer/src/main/java/com/learning/sample/spring/cloud/service/consumer/SampleServiceConsumerApplication.java
+++ spring.cloud.eureka.client.service.consumer/src/main/java/com/learning/sample/spring/cloud/service/consumer/SampleServiceConsumerApplication.java
@@ -7,7 +7,6 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
-@EnableEurekaClient
 @EnableDiscoveryClient
 @SpringBootApplication
 public class SampleServiceConsumerApplication{
